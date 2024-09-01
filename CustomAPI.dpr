6ee4a607f5ea931450a1c6ef4fad5eb47ddedc4a
Aleph Costa Melo <alephcostamelo@gmail.com>
2024-09-01 19:54:24 -0300

    Removendo codigo utils e movendo para outro pacote

--- CustomAPI.dpr
+++ CustomAPI.dpr
@@ -7,28 +7,9 @@
 uses
   SysUtils,
   Classes,
-  UGenericUtils in 'src\utils\UGenericUtils.pas',
-  UWindowsUtils in 'src\utils\UWindowsUtils.pas',
-  UApp in 'src\utils\app\UApp.pas',
-  UAppAPIRequest in 'src\utils\app\api\UAppAPIRequest.pas',
-  UAppAPI in 'src\utils\app\api\UAppAPI.pas',
-  UFileUtils in 'src\utils\UFileUtils.pas',
-  UArrayUtils in 'src\utils\UArrayUtils.pas',
-  UArrayHelper in 'src\utils\models\UArrayHelper.pas',
-  UGenericException in 'src\utils\models\UGenericException.pas',
-  UThreadUtils in 'src\utils\UThreadUtils.pas',
-  UJSONUtils in 'src\utils\UJSONUtils.pas',
-  UJSONException in 'src\utils\models\UJSONException.pas',
-  UStringUtils in 'src\utils\UStringUtils.pas',
-  UDebugUtils in 'src\utils\UDebugUtils.pas',
-  UGenericDictionary in 'src\utils\models\UGenericDictionary.pas',
-  UThreadData in 'src\utils\models\UThreadData.pas',
-  UNTDLL in 'src\utils\UNTDLL.pas',
-  UEnum in 'src\utils\models\UEnum.pas',
-  UEnumUtils in 'src\utils\UEnumUtils.pas',
-  UEnumException in 'src\utils\models\UEnumException.pas',
-  UAttributesUtils in 'src\utils\UAttributesUtils.pas',
-  UEnd in 'src\utils\models\UEnd.pas';
+  UWindowsUtils,
+  UThreadUtils,
+  UThreadData;
 
 procedure onExit;
 begin
@@ -36,12 +17,10 @@ begin
   IsConsole := False;
 end;
 
-var
-  FApp: TApp;
-
+//var
+//  FApp: TApp;
 begin
   AddExitProc(@onExit);
-
 //  TThreadUtils.onThread('ThreadTest', 250, 0,
 //  procedure
 //  var
@@ -50,14 +29,8 @@ begin
 //    TWindowsUtils.cleanScreen;
 //    FThreadData := TThreadUtils.getThreadData('ThreadTest');
 //    Writeln(FThreadData.toString);
+//    FThreadData.Loop := False;
 //  end);
-
-  FApp := TApp.Create;
-  FApp.API('test').&End.API('');
-//    .GET<String>('path-1',
-//    function(AReq: THorseRequest; ARes: THorseResponse): String
-//    begin
-//    end);
 
   sleep(120*1000);
 end.
